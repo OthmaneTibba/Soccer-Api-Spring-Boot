7d636aa4060df14cd8a06969c4e6534a9c98fb70
OthmaneTibba <medtibba@gmail.com>
2023-06-07 00:07:04 +0100

    first commit

--- src/main/java/com/api/soccer/controller/FixtureController.java
+++ src/main/java/com/api/soccer/controller/FixtureController.java
@@ -2,13 +2,14 @@ package com.api.soccer.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FixtureController {
 
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     String test(){
         return "Hello";
     }
