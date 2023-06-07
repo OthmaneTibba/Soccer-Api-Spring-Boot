2b27b532fae0ffc0b660eb591a20b807334f5dfa
OthmaneTibba <medtibba@gmail.com>
2023-06-07 01:06:36 +0100

    first commit

--- src/main/java/com/api/soccer/SoccerApplication.java
+++ src/main/java/com/api/soccer/SoccerApplication.java
@@ -2,11 +2,18 @@ package com.api.soccer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SoccerApplication {
+public class SoccerApplication  extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(SoccerApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SoccerApplication.class);
 	}
 
 }
